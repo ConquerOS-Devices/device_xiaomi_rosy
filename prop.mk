79faebe5a0ec756532129d95bc903d0c436e5eb2
dracarys18 <karthihegde010@gmail.com>
2020-05-05 01:26:34 +0530

    rosy: enable frameworks boost

--- prop.mk
+++ prop.mk
@@ -105,6 +105,18 @@ persist.qfp=false
 PRODUCT_PROPERTY_OVERRIDES += \
 ro.fm.transmitter=false
 
+# Framework boost
+PRODUCT_PROPERTY_OVERRIDES += \
+vendor.iop.enable_iop=1 \
+ro.vendor.at_library=true \
+vendor.iop.enable_uxe=1 \
+vendor.perf.iop_v3.enable=true \
+vendor.perf.iop_v3.enable.debug=false \
+vendor.enable.prefetch=false \
+vendor.iop.enable_prefetch_ofr=false \
+vendor.perf.gestureflingboost.enable=true \
+vendor.perf.workloadclassifier.enable=true
+
 # Frp
 PRODUCT_PROPERTY_OVERRIDES += \
 ro.frp.pst=/dev/block/bootdevice/by-name/config
